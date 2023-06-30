02b3a4c38e1991d80b63e06d07f33bd70d34d0ca
Liban-Madar <liiban333@gmail.com>
2023-06-30 18:42:08 -0500

    created frontend

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+const path = require('path');
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.use('/openai', require('./routes/ai_routes.js'));
 
