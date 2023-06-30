92a0a91cf287e112c047d2cfb15098cafa3ad454
Liban-Madar <liiban333@gmail.com>
2023-06-30 17:08:30 -0500

    setup express server port 5000

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("This is a test")
+const express = require('express');
+const dotenv = require('dotenv');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
